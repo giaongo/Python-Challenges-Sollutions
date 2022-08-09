1caef7cf89f9a4112dadcd3ec67584445fd91319
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-09 19:07:09 +0300

    Finish Challenge 1 - Draw a square

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.color("red")
+#Challenge 1- Draw a square
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(90)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
