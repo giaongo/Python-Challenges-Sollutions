8acea8d875a90544f5bdc70a16bdde40875c3b5c
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-12 13:11:43 +0300

    Turn off the screen animation and only update screen in the while loop when the snack has moved to new position. Detect collision with snake and food and end the game if the snake hits the walls

--- snack_game/main.py
+++ snack_game/main.py
@@ -1,34 +1,43 @@
-from turtle import Turtle,Screen
+from turtle import Screen
+import time
+
+from scoreboard import ScoreBoard
+from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-snake_body = []
-x_coordinate = 0
-for i in range(3):
-    snake = Turtle(shape="square")
-    print(snake.shapesize())
-    snake.color("white")
-    snake.setx(x_coordinate)
-    x_coordinate -= 20
-    snake_body.append(snake)
+screen.tracer(0)  # this method is to turn off the turtle animation
 
+snake = Snake()
+food = Food()
+score = ScoreBoard()
 
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
+    # Detect collision with snake and food
+    if snake.head.distance(food) < 15:
+        score.update_score()
+        snake.extend()
+        food.move_random()
 
-
-
-
-
-
-
-
-
-
-
-
+    # Detect collision with the wall
+    if snake.head.xcor() > 299 or snake.head.xcor() < -299 or snake.head.ycor() > 299 \
+            or snake.head.ycor() < -299:
+        game_is_on = False
+        score.game_over()
 
 
 
