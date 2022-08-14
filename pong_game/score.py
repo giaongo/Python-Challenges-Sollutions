fe7ef583444557d4f25045b3da74c919c4cfd0b1
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-14 19:07:45 +0300

    Display player name next to the score board

--- pong_game/score.py
+++ pong_game/score.py
@@ -1,16 +1,16 @@
-import turtle
 from turtle import Turtle
 
 
 class ScoreBoard(Turtle):
 
-    def __init__(self, position):
+    def __init__(self, position, player_name):
         super().__init__()
         self.penup()
         self.hideturtle()
         self.color("white")
         self.score = 0
         self.goto(position)
+        self.player_name = player_name
         self.post_score()
 
     def increase_score(self):
@@ -19,7 +19,7 @@ class ScoreBoard(Turtle):
         self.post_score()
 
     def post_score(self):
-        self.write(f"{self.score}", False, align="center", font=("Arial", 30, "normal"))
+        self.write(f"{self.player_name} : {self.score}", False, align="center", font=("Comic Sans MS", 20, "normal"))
 
     def post_game_over(self):
         self.write("Game over", False, align="center", font=("Arial", 30, "normal"))
