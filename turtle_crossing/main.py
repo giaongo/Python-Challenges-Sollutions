687d371d646188a90cca0eedc705439f8a156253
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-15 16:01:37 +0300

    Create a new car, in random y position every 4th time of loop. Clear out the car that is out of left screen

--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -1,25 +1,37 @@
 from turtle import Screen
 import time
-from Player import Player
 
+from cars_list import CarsList
+from player import Player
+
+# Screen setting up
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("white")
 screen.title("Turtle Crossing Game")
 screen.tracer(0)
+screen.colormode(255)
 
 player = Player()
 screen.listen()
 screen.onkeypress(key="Up", fun=player.move_forward)
 
 is_game_on = True
+cars = CarsList()
+number_of_loop = 0
 while is_game_on:
     screen.update()
     time.sleep(0.1)
 
+    if number_of_loop % 3 == 0:
+        cars.create_car()
+
+    for car in cars.cars_list:
+        car.move_forward()
+        if car.xcor() < -300:
+            car.clear()
 
 
-
-
+    number_of_loop += 1
 
 screen.exitonclick()
