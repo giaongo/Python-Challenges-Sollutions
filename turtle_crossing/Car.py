1dcd9507e5ca18fdbeb3bfbbd8391fbf763bb08f
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-15 13:29:41 +0300

    Refactor the code and add move_forward function for car

--- turtle_crossing/Car.py
+++ turtle_crossing/Car.py
@@ -10,12 +10,21 @@ class Car(Turtle):
         self.penup()
         self.turtlesize(stretch_wid=1, stretch_len=2)
         self.random_color()
-        self.rand_x = random.randint(-280, 280)
-        self.rand_y = random.randint(-270, 280)
-        self.goto(self.rand_x, self.rand_y)
+        self.random_position()
 
     def random_color(self):
         rand_r = random.randint(0, 255)
         rand_g = random.randint(0, 255)
         rand_b = random.randint(0, 255)
         self.color(rand_r, rand_g, rand_b)
+
+    def random_position(self):
+        rand_x = random.randint(-280, 280)
+        rand_y = random.randint(-270, 280)
+        self.goto(rand_x, rand_y)
+
+    def move_forward(self):
+        self.setheading(180)
+        if self.xcor() > -300:
+            new_x = self.xcor() + 20
+            self.setx(new_x)
