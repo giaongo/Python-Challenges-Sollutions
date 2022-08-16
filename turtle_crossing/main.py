2df961fb6e18358354b852506174772c1475d49d
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-16 09:58:04 +0300

    Change the loop for car moving. Turtle goes back to origin and level increases when turtle reaches the top

--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 
 from cars_list import CarsList
+from level import Level
 from player import Player
 
 # Screen setting up
@@ -18,20 +19,36 @@ screen.onkeypress(key="Up", fun=player.move_forward)
 
 is_game_on = True
 cars = CarsList()
+game_level = Level()
+
 number_of_loop = 0
 while is_game_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(game_level.car_speed)
+
+    if player.ycor() >= 290:
+        game_level.increase_level()
+        player.return_to_origin()
 
     if number_of_loop % 3 == 0:
         cars.create_car()
 
-    for car in cars.cars_list:
-        if player.distance(car) < 18:
+    # Loop for car moving on the screen
+    index = 0
+    while index < len(cars.cars_list):
+        car = cars.cars_list[index]
+
+        if player.distance(car) < 17:
+            game_level.post_game_over()
             is_game_on = False
         car.move_forward()
+
         if car.xcor() < -300:
-            car.clear()
+            car.hideturtle()
+            cars.remove_car(car)
+            index -= 1
+        else:
+            index += 1
 
     number_of_loop += 1
 
--- turtle_crossing/player.py
+++ turtle_crossing/player.py
@@ -14,8 +14,6 @@ class Player(Turtle):
     def move_forward(self):
         new_y = self.ycor() + 10
         self.sety(new_y)
-        if new_y >= 290:
-            self.return_to_origin()
 
     def return_to_origin(self):
         self.goto(x=0, y=-280)
