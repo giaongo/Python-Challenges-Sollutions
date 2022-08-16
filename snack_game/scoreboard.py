cf2c6ea11c022244981d6d63720e1d4b4eddc3f4
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-16 16:21:38 +0300

    Remove Game Over text on the screen. Add the high score attribute which saves the player's high score to data.txt and read that score every time the program re-runs

--- snack_game/scoreboard.py
+++ snack_game/scoreboard.py
@@ -4,13 +4,18 @@ FONT = ("Courier", 18, "normal")
 GAME_OVER_FONT = ("Courier", 30, "normal")
 
 
+def read_high_score():
+    with open("data.txt") as file:
+        return int(file.read())
+
+
 class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
         screen = Screen()
         screen.colormode(255)
-        self.high_score = 0
+        self.high_score = read_high_score()
         self.goto(0, 260)
         self.penup()
         self.color(252, 190, 3)
@@ -24,15 +29,15 @@ class ScoreBoard(Turtle):
 
     def post_score(self):
         self.clear()
-        self.write(f"Total score : {self.score} High Score: {self.high_score}", False, align="center", font=FONT)
+        self.write(f"Total score: {self.score} -- High Score: {self.high_score}", False, align="center", font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+        self.write_high_score()
         self.score = 0
         self.post_score()
 
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.color("red")
-    #     self.write("GAME OVER!!!!!", align="center", font=GAME_OVER_FONT)
+    def write_high_score(self):
+        with open("data.txt", "w") as file:
+            file.write(f"{self.high_score}")
