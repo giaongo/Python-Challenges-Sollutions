31038830cdc28e725a8330b49ae7aabc28c027c3
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-18 12:07:57 +0300

    Create UserGuess class that has functions (retrieve_data() and write_to_map())

---
+++ us_state_game/user_guess.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class UserGuess(Turtle):
+
+    def __init__(self, answer_state):
+        super().__init__()
+        self.answer_state = answer_state
+        self.penup()
+        self.hideturtle()
+        self.color("black")
+
+    def retrieve_data(self, data_frame):
+        found_answer_series = data_frame[data_frame.state == self.answer_state]
+        x_coordinate = int(found_answer_series.x)
+        y_coordinate = int(found_answer_series.y)
+        return x_coordinate, y_coordinate
+
+    def write_to_map(self, coordinate):
+        self.clear()
+        self.goto(coordinate)
+        self.write(f"{self.answer_state}", align="center", font=("Arial", 8, "bold"))
+
