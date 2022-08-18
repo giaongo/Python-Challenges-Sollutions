c3bbda34845f538d5f6cf09163676fa4e3b8f658
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-18 12:09:12 +0300

    Create a loop that looks for user input from data_frame and writes to map if guess is found.

--- us_state_game/main.py
+++ us_state_game/main.py
@@ -2,6 +2,8 @@ import turtle
 from turtle import Screen
 import pandas
 
+from user_guess import UserGuess
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("U.S States Game")
@@ -10,12 +12,28 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+correct_guess = []
+score = 0
+game_is_on = True
+while game_is_on:
+    answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?")
+    answer_state = answer_state.title()
+    user_guess = UserGuess(answer_state)
+
+    if answer_state in data.values:
+        if answer_state not in correct_guess:
+            coordinate = user_guess.retrieve_data(data)
+            user_guess.write_to_map(coordinate)
+            correct_guess.append(answer_state)
+            score += 1
+        else:
+            print("You have already guessed this word before")
+    else:
+        print("data not found")
 
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
-
-
-turtle.onscreenclick(get_mouse_click_coor)
 turtle.mainloop()
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
