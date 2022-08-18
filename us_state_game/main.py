90bcf0e5b3593a707c39fd19bc67181c11bb4f59
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-18 08:37:25 +0300

    Setting up the screen and request the user input

--- us_state_game/main.py
+++ us_state_game/main.py
@@ -1,1 +1,21 @@
+import turtle
+from turtle import Screen
+import pandas
 
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+
+
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
