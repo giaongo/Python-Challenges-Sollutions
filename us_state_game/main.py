0018ad8a28d6d919a56f7090c36d7090aef8d02b
Giao Ngo <giao.ngo@metropolia.fi>
2022-08-18 13:25:45 +0300

    Add more justified comments. Update the name of class and its object

--- us_state_game/main.py
+++ us_state_game/main.py
@@ -1,9 +1,9 @@
 import turtle
 from turtle import Screen
 import pandas
+from player_guess import PlayerGuess
 
-from user_guess import UserGuess
-
+# Setting up the screen and append image background
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("U.S States Game")
@@ -14,22 +14,25 @@ turtle.shape(image)
 data = pandas.read_csv("50_states.csv")
 all_state_name = data["state"].tolist()
 correct_guess = []
+
+# Game starts
 while len(correct_guess) < 50:
     answer_state = screen.textinput(title=f"{len(correct_guess)}/50 States Correct", prompt="What's another state's "
                                                                                             "name?").title()
     if answer_state == "Exit":
         break
-    user_guess = UserGuess(answer_state)
+    player_guess = PlayerGuess(answer_state)
     if answer_state in data.values:
         if answer_state not in correct_guess:
-            coordinate = user_guess.retrieve_data(data)
-            user_guess.write_to_map(coordinate)
+            coordinate = player_guess.retrieve_data(data)
+            player_guess.write_to_map(coordinate)
             correct_guess.append(answer_state)
         else:
-            print("You have already guessed this word before")
+            player_guess.notify_player("This state has been guessed before")
     else:
-        print("data not found")
+        player_guess.notify_player("No data found")
 
+# Game exits and all remaining states will be written to a csv file
 remained_states = []
 for state in all_state_name:
     if state not in correct_guess:
